fec39163d8bb9e04a2b968dccea5768ffab56e88
kayuh <kayuh@kayuhs-MacBook-Air.local>
2023-02-20 10:13:10 -0800

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export const Home = () => {
 
     const posts = [
@@ -30,6 +30,23 @@ export const Home = () => {
         ];
 
     return (
-        <div>Home</div>
+        <div className="home">
+            <div className="posts">
+                {posts.map((post) => (
+                    <div className="post" key = {post.id}>
+                        <div className="img">
+                            <img src={post.img} alt="img"/>
+                        </div>
+                        <div className="content">
+                            <Link className="link" to={`/post/${post.id}`}>
+                                <h1>{post.title}</h1>
+                                <p>{post.desc}</p>
+                                <button>Read More</button>
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
